90ef622b16f27238cf5c25e307a37c02740663d5
Mustafa Ibrahim <Mustafa7Ibrahim@gmail.com>
2022-12-11 12:06:09 +0200

    mm

--- lib/custom_dio.dart
+++ lib/custom_dio.dart
@@ -1,11 +1,10 @@
 library custom_dio;
 
-import 'package:custom_dio/get_it.dart';
 import 'package:custom_dio/hive_helper/hive_helper.dart';
 
 /// A Calculator.
 class CustomDio {
   CustomDio(baseUrl) {
-    gi<HiveDioHelper>().putData('baseURL', baseUrl);
+    HiveDioHelper().putData('baseURL', baseUrl);
   }
 }
